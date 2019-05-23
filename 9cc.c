39af4fe269ba32907d954bb106614c1b3cd56198
s-now <k838450@gmail.com>
2019-05-23 05:51:26 -0700

    add tokenize

--- 9cc.c
+++ 9cc.c
@@ -1,34 +1,116 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<ctype.h>
+#include<stdarg.h>
+#include<string.h>
+
+enum{
+	TK_NUM = 256,
+	TK_EOF,
+};
+
+typedef struct{
+	int ty;
+	int val;
+	char *input;
+}Token;
+
+
+char *user_input;
+
+Token tokens[100];
+
+
+void error(char *fmt, ...){
+	va_list ap;
+	va_start(ap,fmt);
+	vfprintf(stderr,fmt,ap);
+	fprintf(stderr,"\n");
+	exit(1);
+}
+
+
+void error_at(char *loc,char *msg){
+	int pos = loc - user_input;
+	fprintf(stderr,"%s\n",user_input);
+	fprintf(stderr,"%s",pos, "");
+	fprintf(stderr,"^ %s\n",msg);
+	exit(1);
+}
+
+
+void tokenize(){
+	char *p = user_input;
+
+	int i = 0;
+	while(*p){
+		if(isspace(*p)){
+			p++;
+			continue;
+		}
+
+		if(*p == '+' || *p == '-'){
+			tokens[i].ty = *p;
+			tokens[i].input = p;
+			i++;
+			p++;
+			continue;
+		}
+
+		if(isdigit(*p)){
+			tokens[i].ty = TK_NUM;
+			tokens[i].input = p;
+			tokens[i].val = strtol(p,&p,10);
+			i++;
+			continue;
+		}
+
+		error_at(p,"cannot tokenize");
+	}
+
+	tokens[i].ty = TK_EOF;
+	tokens[i].input = p;
+}
+
 
 int main(int argc,char **argv){
 	if(argc != 2){
-		fprintf(stderr,"no correct argument");
+		error("no correct argument");
 		return 1;
 	}
 	
-	char *p = argv[1];
+	user_input = argv[1];
+	tokenize();
 
 	printf(".intel_syntax noprefix\n");
 	printf(".global main\n");
 	printf("main:\n");
-	printf("  mov rax, %d\n",strtol(p,&p,10));
 
-	while (*p){
-		if(*p == '+'){
-			p++;
-			printf("  add rax,%ld\n",strtol(p,&p,10));
+	if(tokens[0].ty != TK_NUM)
+		error_at(tokens[0].input,"no figure");
+	printf("  mov rax, %d\n",tokens[0].val);
+
+	int i = 1;
+	while (tokens[i].ty != TK_EOF){
+		if(tokens[i].ty == '+'){
+			i++;
+			if(tokens[i].ty != TK_NUM)
+				error_at(tokens[i].input,"no figure");
+			printf("  add rax,%ld\n",tokens[i].val);
+			i++;
 			continue;
 		}
 
-		if(*p == '-'){
-			p++;
-			printf("  sub rax,%ld\n",strtol(p,&p,10));
+		if(tokens[i].ty == '-'){
+			i++;
+			if(tokens[i].ty != TK_NUM)
+				error_at(tokens[i].input,"no figure");
+			printf("  sub rax,%ld\n",tokens[i].val);
+			i++;
 			continue;
 		}
 
-		fprintf(stderr,"unexpexted charactor: '%c'\n",*p);
-		return 1;
+		error_at(tokens[i].input,"unexpexted token");
 	}
 
 	printf("  ret\n");
